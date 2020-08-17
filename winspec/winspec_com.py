712a47b481a558403b0380a2b03671ba7d9be8a2
John Jarman <johncjarman@gmail.com>
2020-08-18 00:21:22 +0100

    Try adding spectrum acquisition with WinX32

--- winspec/winspec_com.py
+++ winspec/winspec_com.py
@@ -8,7 +8,15 @@ John Jarman <jcj27@cam.ac.uk>
 import logging
 import threading
 import time
+import numpy as np
 from winspec.exceptions import WinspecError, WinspecErrorCodes
+
+import comtypes.client
+import win32com.client as win32
+import ctypes
+
+comtypes.client.GetModule(('{1A762221-D8BA-11CF-AFC2-508201C10000}', 3, 11))
+import comtypes.gen.WINX32Lib as WinSpecLib
 
 class WinspecCOM:
     """
@@ -40,7 +48,32 @@ class WinspecCOM:
                                'Unable to start acquisition due to concurrent operation')
         try:
             logging.info('Acquire spectrum')
-            time.sleep(25)
+            wx32_expt = win32.Dispatch("WinX32.ExpSetup")
+            wx32_doc = win32.Dispatch("WinX32.DocFile")
+            wx32_expt.Start(wx32_doc)
+
+            while wx32_expt.GetParam(WinSpecLib.EXP_RUNNING)[0]:
+                time.sleep(1)
+
+            raw_spectrum_buffer = ctypes.c_uint16()
+
+            wx32_doc.GetFrame(1, raw_spectrum_buffer)
+
+            raw_spectrum = np.ctypeslib.as_array(raw_spectrum_buffer)
+            
+            spectrum = np.empty((2, len(raw_spectrum)))
+            spectrum[1] = spectrum
+            calibration = wx32_doc.GetCalibration()
+            
+            poly_coeffs = np.array([])
+
+            for i in range(calibration.Order + 1):
+                np.insert(poly_coeffs, 0, calibration.PolyCoeffs(i))
+
+            spectrum[0] = np.polyval(poly_coeffs, range(1, 1+len(raw_spectrum)))
+            
             logging.info('Acquire complete')
+            return spectrum
+        
         finally:
             self._lock.release()
